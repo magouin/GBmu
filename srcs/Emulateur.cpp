45982cd70ce5c7650fc18d1b8fb0923bab2f07a9
Maxime Gouin <magouin@student.42.fr>
2019-06-11 16:42:00 +0200

    ca avance
    Former-commit-id: 45eb67a2ac0e9278414166b95c9d6f7cfd47fcdf

--- srcs/Emulateur.cpp
+++ srcs/Emulateur.cpp
@@ -24,6 +24,26 @@ Emulateur::Emulateur(std::string rom): _ROM(rom)
 	bzero(_RAM, sizeof(_RAM));
 }
 
+void	Emulateur::print_regs(void)
+{
+	printf("\n--- REGISTERS ---\n");
+	printf("B: 0x%02hhx\t\tC: 0x%02hhx\n", this->regs.bc.bc.B, this->regs.bc.bc.C);
+	printf("D: 0x%02hhx\t\tE: 0x%02hhx\n", this->regs.de.de.D, this->regs.de.de.E);
+	printf("H: 0x%02hhx\t\tL: 0x%02hhx\n", this->regs.hl.hl.H, this->regs.hl.hl.L);
+	printf("A: 0x%02hhx\n", this->regs.af.af.A);
+	printf("SP: 0x%04hx\tPC: 0x%04hx\n", this->regs.SP, this->regs.PC);
+	printf("F =");
+	if (this->regs.af.af.F & FLAG_Z)
+		printf(" FLAG_Z");
+	if (this->regs.af.af.F & FLAG_H)
+		printf(" FLAG_H");
+	if (this->regs.af.af.F & FLAG_N)
+		printf(" FLAG_N");
+	if (this->regs.af.af.F & FLAG_CY)
+		printf(" FLAG_CY");
+	printf("\n");
+}
+
 void Emulateur::emu_start(uint32_t begin, uint32_t end)
 {
 	printf("Starting emulation from 0x%X to 0x%X\n", begin, end);
@@ -45,6 +65,7 @@ void Emulateur::emu_start(uint32_t begin, uint32_t end)
 
 		this->regs.PC += 1 + instr->nb_params * 1;
 		instr->f();
+		print_regs();
 		std::cout << std::endl;
 	}
 }
