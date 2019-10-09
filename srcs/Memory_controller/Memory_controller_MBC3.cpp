8e19d3abb266dee98da28dffbc04e541b44144dc
Maxime Gouin <magouin@student.42.fr>
2019-10-09 21:01:33 +0200

    opti
    Former-commit-id: 32dab0a68c492606ace8a75e84ebeeaf874cc5da [formerly 32dab0a68c492606ace8a75e84ebeeaf874cc5da [formerly 70735e34cc2c7078452ac5a2711df5e8cd5d603f]]
    Former-commit-id: 7c761e35696a9525f522f3bed35222b41ff88ba4
    Former-commit-id: 2080d4b23ab8f8df5a7e6e98703a2cd1b2c4a17b

--- srcs/Memory_controller/Memory_controller_MBC3.cpp
+++ srcs/Memory_controller/Memory_controller_MBC3.cpp
@@ -78,7 +78,7 @@ bool		Memory_controller_MBC3::write_RAM_regs(uint8_t *addr, uint16_t value, int8
 	return (false);
 }
 
-Memory_controller_MBC3::Memory_controller_MBC3(Emulateur &emu, size_t ram_size): Memory_controller(emu, ram_size)
+Memory_controller_MBC3::Memory_controller_MBC3(Emulateur &emu, size_t ram_size, bool debug): Memory_controller(emu, ram_size, debug)
 {
 	init(ram_size);
 	_ROM_RAM_BANK = 0;
