f38f2206f48fc354c2e6b833d00899a7f0030e7b
vsteffen <vsteffen@student.42.fr>
2019-10-16 21:18:25 +0200

    Rename variable inside Memory Controller
    Former-commit-id: 453b9d728e1cd35664a2a24e1a5697b267a37902

--- srcs/Memory_controller/Memory_controller_MBC5.cpp
+++ srcs/Memory_controller/Memory_controller_MBC5.cpp
@@ -10,8 +10,8 @@ void		*Memory_controller_MBC5::read_ROM_RAM_regs(uint8_t *addr)
 		return (void*)(rom_bank + (addr - _emu.RAM - 0x4000));
 	else if (addr - _emu.RAM >= 0xa000 && addr - _emu.RAM  < 0xc000)
 	{
-		if (_RAM_ENABLE)
-			return (void*)(ram_bank + (addr - _emu.RAM - 0xa000));
+		if (_ram_ext_work_enable)
+			return (void*)(ram_ext_work_bank + (addr - _emu.RAM - 0xa000));
 		return (NULL);
 	}
 	else return (NULL);
@@ -23,23 +23,23 @@ bool		Memory_controller_MBC5::write_ROM_regs(uint8_t *addr, uint8_t value, int8_
 		return (false);
 
 	if (addr - _emu.RAM < 0x2000)
-		_RAM_ENABLE = ((value & 0xf) == 0xa ? true : false);
+		_ram_ext_work_enable = ((value & 0xf) == 0xa ? true : false);
 	else if (addr - _emu.RAM < 0x3000)
 	{
-		_ROM_BANK = value;
+		_rom_bank_selected = value;
 	}
 	else if (addr - _emu.RAM < 0x4000)
 	{
-		_ROM_BANK = value & 1;
+		_rom_bank_selected = value & 1;
 	}
 	else if (addr - _emu.RAM < 0x6000)
 	{
-		_RAM_BANK = value & 0xf;
+		_ram_ext_work_bank_to_select = value & 0xf;
 	}
 	else
 		return (false);
-	rom_bank = (const uint8_t*)(_emu.ROM.c_str() + 0x4000 * _ROM_BANK);
-	ram_bank = external_ram + _RAM_BANK * 0x2000;
+	rom_bank = (const uint8_t*)(_emu.ROM.c_str() + 0x4000 * _rom_bank_selected);
+	ram_ext_work_bank = ram_ext_work_orig_ptr + _ram_ext_work_bank_to_select * 0x2000;
 	return (true);
 }
 
@@ -48,9 +48,9 @@ bool		Memory_controller_MBC5::write_RAM_regs(uint8_t *addr, uint16_t value, int8
 	if (addr - _emu.RAM  >= 0xA000 && addr - _emu.RAM  < 0xC000)
 	{
 		if (size == 2)
-			*(uint16_t *)(ram_bank + (addr - _emu.RAM - 0xA000)) = value;
+			*(uint16_t *)(ram_ext_work_bank + (addr - _emu.RAM - 0xA000)) = value;
 		else
-			*(uint8_t *)(ram_bank + (addr - _emu.RAM - 0xA000)) = (uint8_t)value;
+			*(uint8_t *)(ram_ext_work_bank + (addr - _emu.RAM - 0xA000)) = (uint8_t)value;
 		return (true);
 	}
 	return (false);
