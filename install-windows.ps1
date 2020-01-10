0edf308a10ce9be36f0f524b20cc1d25dbb08646
Maxime Gouin <magouin@student.42.fr>
2020-01-10 15:31:38 +0100

    resources

--- install-windows.ps1
+++ install-windows.ps1
@@ -1,6 +1,8 @@
 msbuild .\Windows\Gbmu.sln
+rcc -o Qt/resources.rcc -binary Qt/windows_resources.qrc
 qmake Qt
 mingw32-make.exe
 mingw32-make.exe clean
 Remove-Item Makefile
 Remove-Item .qmake.stash
+pause
